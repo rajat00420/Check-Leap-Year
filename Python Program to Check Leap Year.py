93f18552077f1d2845f26ca7e626e57d4f78af44
rajat00420 <rajat.khajuria@my.jcu.edu.au>
2020-10-26 14:48:28 +0530

    Add files via upload

---
+++ Python Program to Check Leap Year.py
@@ -0,0 +1,17 @@
+year  = int(input("enter your year:"))
+
+if year %4 == 0:
+    if year %100 == 0:
+        if year %400 == 0:
+            print("{0} is a leap year".format(year))
+        else:
+            print("{0} is not a leap year".format(year))
+
+    else:
+        print("{0} is a leap year".format(year))
+
+else:
+    print("{0} is not a leap year".format(year))
+
+
+
